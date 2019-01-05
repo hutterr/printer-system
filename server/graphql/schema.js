9cfc57ab47927b286855cf6c1f4410bcece3f27e
Robert Hutter <hutterrbert@gmail.com>
2019-01-05 15:55:50 +0100

    schema elkezdése

---
+++ server/graphql/schema.js
@@ -0,0 +1,58 @@
+const { buildSchema } = require("graphql");
+
+module.exports = buildSchema(`
+    type User {
+        _id : ID!
+        email: String!
+        password: String
+    }
+    type Szamlalo{
+        _id: ID!
+        datum: String!
+        color: String
+        bk: String!
+    }
+    type Alkatresz{
+        _id: ID!
+        edp: String!
+        db: String!
+    }
+    type Javitasok{
+        _id: ID!
+        datum: String!
+        szervizes: User!
+        leiras: String!
+        alkatresz: [Alkatresz!]!
+    }
+    type Printer{
+        _id: ID!
+        gepszam: String!
+        marka: String!
+        tipus: String!
+        sn: String!
+        szamlalo: [Szamlalo!]!
+        javitasok: [Javitasok!]!
+    }
+    input PrinterInput{
+        gepszam: String!
+        marka: String!
+        tipus: String!
+        sn: String!
+    }
+    input UserInput {
+        email: String!
+        password: String!
+    }
+    type RootQuery{
+        printers: [Printer!]!
+    
+    }
+    type RootMutation{
+        createPrinter(printerInput: PrinterInput): Printer
+        createUser(userInput: UserInput): User
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
